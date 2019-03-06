2bcd0a6ff14128571a5715eb5bd53ebe00ddbd8d
Joerg Kritzen <43033899+jkritzen@users.noreply.github.com>
2019-03-06 13:35:15 +0100

    Update nsg.tf

--- terraform-azure-dev/nsg.tf
+++ terraform-azure-dev/nsg.tf
@@ -1,3 +1,21 @@
+resource "azurerm_network_security_group" "NSG_DMZ" {
+  name                = "NSG_DMZ"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.MyRG.name}"
+  tags                = "${var.tags}"
+
+  security_rule {
+    name                       = "allow_80"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  
 resource "azurerm_network_security_group" "NSG_WEB" {
   name                = "NSG_WEB"
   location            = "${var.location}"
